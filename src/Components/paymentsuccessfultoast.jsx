ae645e6e05e228de9869b5b03604a54d98af365e
Akib Ali <akib7599@gmail.com>
2023-01-20 17:18:16 +0530

    done successful payment toast

--- src/Components/paymentsuccessfultoast.jsx
+++ src/Components/paymentsuccessfultoast.jsx
@@ -1,9 +1,30 @@
+import { useToast,Wrap,Button} from '@chakra-ui/react'
+import React from 'react'
+
 export const PaymentToastSuccessful=()=>{
+    const toast = useToast(true)
+
 
 return(
 
-    <div>
-        <h1>payment successuful</h1>
+    <div style={{ width:"300px" , margin:"auto" ,marginTop:"50px"}}>
+        {/* <h1  style={{textAlign:"center"}}>Confirm To Payment</h1> */}
+        <Button
+      colorScheme='teal' size="lg"
+        onClick={() =>
+          toast({
+            title: 'Successfully payment.',
+            description: "Congratulation Your Payment is successful",
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+            position:"top"
+          })
+        }
+      >
+        Cofirm to Payment
+      </Button>
+
     </div>
 )
 
